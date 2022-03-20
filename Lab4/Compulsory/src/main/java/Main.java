61c4c8a7125a5c5c258e41da0869d740a20963fa
AlexandraVolentir <alexandra.volentir123@gmaul.com>
2022-03-20 20:30:15 +0200

    Lab4 Compulsory

--- Lab4/Compulsory/src/main/java/Main.java
+++ Lab4/Compulsory/src/main/java/Main.java
@@ -1,5 +1,65 @@
+import java.util.*;
+import java.util.stream.IntStream;
+
+/**
+ * main class of the program
+ */
 public class Main {
+    /**
+     * here is where the main code will be executed
+     */
     public static void main(String[] args) {
-        System.out.println("Hello");
+        //The simple way
+        Intersection[] nodes = IntStream.rangeClosed(0, 8) .mapToObj(i -> new Intersection("vertice" + i) ) .toArray(Intersection[]::new);
+        List<Intersection> nodesList =
+                new ArrayList<Intersection>(Arrays.asList(nodes));
+
+        Street street0 = new Street("street0", 2, nodesList.get(0), nodesList.get(1));
+        Street street1 = new Street("street1", 3, nodesList.get(1), nodesList.get(2));
+        Street street2 = new Street("street2", 1, nodesList.get(2), nodesList.get(3));
+        Street street3 = new Street("street3", 1, nodesList.get(3), nodesList.get(5));
+        Street street4 = new Street("street4", 1, nodesList.get(3), nodesList.get(4));
+        Street street5 = new Street("street5", 2, nodesList.get(2), nodesList.get(5));
+        Street street6 = new Street("street6", 1, nodesList.get(4), nodesList.get(5));
+        Street street7 = new Street("street7", 3, nodesList.get(5), nodesList.get(6));
+        Street street8 = new Street("street8", 3, nodesList.get(6), nodesList.get(7));
+        Street street9 = new Street("street9", 1, nodesList.get(7), nodesList.get(8));
+        Street street10 = new Street("street10", 2, nodesList.get(6), nodesList.get(8));
+        Street street11 = new Street("street11", 2, nodesList.get(4), nodesList.get(8));
+        Street street12 = new Street("street12", 2, nodesList.get(0), nodesList.get(7));
+        Street street13 = new Street("street13", 2, nodesList.get(0), nodesList.get(8));
+
+        ArrayList<Street> listStreets = new ArrayList<>();
+        listStreets.add(street0);
+        listStreets.add(street1);
+        listStreets.add(street2);
+        listStreets.add(street3);
+        listStreets.add(street4);
+        listStreets.add(street5);
+        listStreets.add(street6);
+        listStreets.add(street7);
+        listStreets.add(street8);
+        listStreets.add(street9);
+        listStreets.add(street10);
+        listStreets.add(street11);
+        listStreets.add(street12);
+        listStreets.add(street13);
+
+//        listStreets.sort(
+//                (Street streetNr1, Street streetNr2) ->
+//                        streetNr1.getLength().compareTo(streetNr2.getLength()));
+//        );
+        
+        listStreets.forEach(System.out::println);
+        listStreets.sort(Street::compareStreetsByLength);
+        System.out.println();
+        listStreets.forEach(System.out::println);
+        System.out.println();
+
+        // check the properties of the hashset
+        Set<Intersection> setOfIntersections = new HashSet<>(nodesList);
+        setOfIntersections.add(nodesList.get(0));
+        setOfIntersections.add(nodesList.get(0));
+        setOfIntersections.forEach(System.out::println);
     }
 }
--- Lab4/Compulsory/src/main/java/Street.java
+++ Lab4/Compulsory/src/main/java/Street.java
@@ -1,53 +1,132 @@
 import java.util.*;
 import java.util.stream.Collectors;
 
+/**
+ * stores data members and methods that make up the street
+ */
 public class Street {
 
     private String name;
-    private String length;
-    List<Intersection> nodeList = new ArrayList<>();
+    private int length;
+    private List<Intersection> nodeList;
+    static ArrayList<Street> listOfStreets = new ArrayList<>();
 
-    public Street(String name, String length) {
+    /**
+     * constructor for street
+     * @param name the name of the street
+     * @param length the length of the street
+     */
+    public Street(String name, int length) {
         this.name = name;
         this.length = length;
+        nodeList = new ArrayList<>();
+        listOfStreets.add(this);
     }
 
+    /**
+     * constructor for street that takes as parameters the names of the two intersections
+     * @param name the name of the street
+     * @param length the length of the street
+     * @param intersection1 the first intersection that has a join with the street
+     * @param intersection2 the second intersection that has a join with the street
+     */
+    public Street(String name, int length, Intersection intersection1, Intersection intersection2) {
+        this.name = name;
+        this.length = length;
+        nodeList = new ArrayList<>();
+        nodeList.add(intersection1);
+        nodeList.add(intersection2);
+        listOfStreets.add(this);
+    }
+
+    /**
+     * getter for the name of the street
+     * @return the name of the street
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * setter for the name of the street
+     * @param name the name of the street
+     */
     public void setName(String name) {
         this.name = name;
     }
 
-    public String getLength() {
+    /**
+     * getter fot the length of the street
+     * @return the length of the street
+     */
+    public int getLength() {
         return length;
     }
 
-    public void setLength(String length) {
+    /**
+     * setter for the length of the street
+     * @param length the length of the street
+     */
+    public void setLength(int length) {
         this.length = length;
     }
 
+    /**
+     * getter for the nodeList
+     * @return
+     */
     public List<Intersection> getNodeList() {
         return nodeList;
     }
 
+    /**
+     * setter for the nodeList
+     * @param nodeList the nodeLsit
+     */
     public void setNodeList(List<Intersection> nodeList) {
         this.nodeList = nodeList;
     }
 
+    /**
+     * function to add a node to the nodeList
+     * @param intersection the intersection object
+     */
     public void addNodes(Intersection intersection){
         nodeList.add(intersection);
     }
 
+    /**
+     * rewrites the nodeList
+     * @param intersections the list with the intersections
+     */
     public void addAll(ArrayList<Intersection> intersections){
         this.nodeList = intersections;
     }
 
-    public void sorting(){
-        Collections.sort(nodeList, ((u,v) -> u.getName().compareTo(v.getName())));;
-//        List<Intersection> newSortedList = nodeList.stream() .sorted(Comparator.comparing(Intersection::getName)) .collect(Collectors.toList());
+    /**
+     * static method to compare the streets
+     * @param street1 the first street of the comparison
+     * @param street2 the second street of the comparison
+     * @return the return value of compare
+     */
+    public static int compareStreetsByLength(Street street1, Street street2) {
+        // note - compare t compares only objects
+        int length1 = street1.getLength();
+        int length2 = street2.getLength();
+        return Integer.compare(length1, length2);
     }
 
-
+    /**
+     * concatenates all the data members of the class
+     * in order for them to be represented as an entity
+     * @return
+     */
+    @Override
+    public String toString() {
+        return "Street{" +
+                "name='" + name + '\'' +
+                ", length=" + length +
+                ", nodeList=" + nodeList +
+                '}';
+    }
 }
