d09632f301c65845d0dc1a771a3cf9c2b7de5583
AlexandraVolentir <alexandra.volentir123@gmaul.com>
2022-03-28 18:39:55 +0300

    First commit Lab6

---
+++ Lab6/src/main/java/MainFrame.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class MainFrame extends JFrame {
+    ConfigPanel configPanel;
+    ControlPanel controlPanel;
+    DrawingPannel canvas;
+    public static void main(String[] args) {
+        System.out.println("Hellli");
+    }
+}
